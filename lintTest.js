f63e54b0721880d4ed6ba965778644cb5930901b
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-06-21 15:08:37 +0900

    Quick update

--- lintTest.js
+++ lintTest.js
@@ -1,2 +1,11 @@
 const    github =                       'super-linter'
   console.log(github)
+
+       function greeting(){
+                                     return 'hello'
+       }
+
+    
+   var greet = greeting
+
+ console.log(                 greet   (    )   )
